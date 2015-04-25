112016211103a1473cce4fa08895165ca0151ca1
Michael Köppl <mko.mko31@gmail.com>
2015-04-25 17:21:00 +0200

    Hauptfenster hinzugefügt und Funktionen hinzugefügt/angefangen
    - Hauptfenster mit einfachem Design angefangen
    - Login leitet jetzt an das neue Hauptfenster weiter

--- BOM/BOM/Form1.cs
+++ BOM/BOM/Form1.cs
@@ -14,13 +14,33 @@ namespace BOM
         public Form1()
         {
             InitializeComponent();
+            input_nutzerName.KeyDown += new KeyEventHandler(enter_KeyDown);
+            input_pwd.KeyDown += new KeyEventHandler(enter_KeyDown);
+        }
+
+        public void enter_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return)
+            {
+                button_Anmelden.PerformClick();
+            }
         }
 
         private void button_Anmelden_Click(object sender, EventArgs e)
         {
             if (input_nutzerName.Text.Length >= 3 && input_pwd.Text.Length >= 6)
             {
-                MessageBox.Show("Uname: " + input_nutzerName.Text + "\tPw: " + input_pwd.Text);
+                if (true /*if username exists and if password is correct*/)
+                {
+                    this.Hide();
+                    var main = new Main();
+                    main.ShowDialog();
+                    this.Close();
+                }
+                else // vorerst unerreichbar
+                {
+                    MessageBox.Show("Der Nutzername oder das Passwort ist falsch.");
+                }
             } else if (input_nutzerName.Text == string.Empty && input_pwd.Text == string.Empty) {
                 MessageBox.Show("Bitte fuellen Sie alle Felder mit Daten.");
             } else if(input_nutzerName.Text.Length < 3){
