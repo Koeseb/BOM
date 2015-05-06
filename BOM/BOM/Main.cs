743308a39d46815205f1de743a4f287376015b9c
Michael Köppl <mko.mko31@gmail.com>
2015-05-06 23:10:41 +0200

    Removed unnecessary SELECT statement on startup

--- BOM/BOM/Main.cs
+++ BOM/BOM/Main.cs
@@ -24,17 +24,6 @@ namespace BOM
             ListBox_ActivityList.Items.Add("Franz Mueller hat 2543 Euro abgebucht (Grund: Kaffeemaschine kaputt).\n");
             ListBox_ActivityList.Items.Add("Ramasan Butaki hat fuer 300 Euro einen Schrank gekauft.\n");
             ListBox_ActivityList.Items.Add("Marus Troester hat 20 Euro abgebucht (Grund: Nach Neuseeland fliegen.)\n");
-
-            try
-            {
-                DBase db = new DBase();
-                //db.insert("INSERT INTO NUTZER (VName, NName, NGeschl, Admin, Akz");
-                db.select("SELECT * FROM NUTZER WHERE NutzerID = 2;");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-            }
         }
 
         private void Label_Rechnungsdatum_Click(object sender, EventArgs e)
