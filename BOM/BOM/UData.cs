5c6304ff0e7c63d515ebfa6fb6dbd671c0ce916f
Michael Köppl <mko.mko31@gmail.com>
2015-05-08 00:06:10 +0200

    Added class to store user related data

---
+++ BOM/BOM/UData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BOM
+{
+    static class UData
+    {
+        public static string Uname;
+        public static int UID;
+    }
+}
